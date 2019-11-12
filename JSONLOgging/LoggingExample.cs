20f45b055347e87274367cafc93331fb52bba29e
Dutta <nilanjan.dutta@siemens-healthineers.com>
2019-11-12 15:51:13 +0530

    Add project files.

---
+++ JSONLOgging/LoggingExample.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace JSONLOgging
+{
+    internal class LoggingExample
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LoggingExample));
+
+        public LoggingExample()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("App1.config"));
+
+            log.Debug("Instantiating Logging Example"+ Environment.NewLine + "Another Line of info");
+        }
+
+        public void LogError()
+        {
+            log.Error("Error Occurred");
+        }
+
+    }
+}
---
+++ JSONLOgging/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JSONLOgging
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            var loggerEx = new LoggingExample();
+            loggerEx.LogError();
+
+            //Console.ReadKey();
+        }
+    }
+}
